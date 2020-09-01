e4a0f39d98c786a8b7e0321072e8c5c66342b294
KacperZiarkowski <PLUJKV3@amway.com>
2020-09-01 16:56:39 +0200

    PriceFormula tests

---
+++ UnitTests/ApplicationCore_Tests/Model/Product/PriceFormulaTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShop_ApplicationCore.Model.Product;
+using eShop_ApplicationCore.Model.Tax;
+using NUnit.Framework;
+
+namespace UnitTests.ApplicationCore_Tests.Model.Product
+{
+    [TestFixture]
+    class PriceFormulaTests
+    {
+        private readonly eShop_ApplicationCore.Model.Product.Product _product =
+            new eShop_ApplicationCore.Model.Product.Product { ProductBasePrice = 12.89m };
+       
+        private readonly Catalog _catalog = new Catalog { PriceModifier = 0.22m };
+        
+        private readonly Vat _tax = new Vat { VatRate = 0.22m };
+        
+
+        [Test]
+
+    }
+}
