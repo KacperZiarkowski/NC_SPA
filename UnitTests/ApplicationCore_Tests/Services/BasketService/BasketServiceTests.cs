21997a6c28a1f2399b20718cec687c70f7ff0df0
KacperZiarkowski <PLUJKV3@amway.com>
2020-09-08 17:22:07 +0200

    Application core

---
+++ UnitTests/ApplicationCore_Tests/Services/BasketService/BasketServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests.ApplicationCore_Tests.Services.BasketService
+{
+    [TestFixture]
+    class BasketServiceTests
+    {
+    }
+}
--- eShop_Application Core/Model/Interfaces/DbContext/IEShopDbContext.cs
+++ eShop_Application Core/Model/Interfaces/DbContext/IEShopDbContext.cs
@@ -1,13 +1,18 @@
-﻿namespace eShop_ApplicationCore.Model.Interfaces.DbContext
+﻿using System.Threading.Tasks;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using eShop_ApplicationCore.Model;
+using eShop_ApplicationCore.Model.Interfaces.DbContext;
+
+
+namespace eShop_ApplicationCore.Model.Interfaces.DbContext
 {
-  using System;
-  using System.Data.Entity;
-  using System.Linq;
-  using System.Linq.Expressions;
+    public interface IEShopDbContext : IProductDbContext, IOrderDbContext, ITaxDbContext, IBasketDbContext
+    {
+        Task<T> GetByIdAsync<T>(int id) where T : Entity;
 
-
-  public interface IeShopDbContext : IProductDbContext, IOrderDbContext, ITaxDbContext
-  {
-
-  }
+    }
 }
