1a999f5350b6760bc7fd052257d31ced0057382c
KacperZiarkowski <PLUJKV3@amway.com>
2020-09-15 14:57:04 +0200

    Integration test

--- IntegrationTests/Repositories/BasketRepositoryTests/QuantitiesTests.cs
+++ IntegrationTests/Repositories/BasketRepositoryTests/QuantitiesTests.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using eShop_ApplicationCore.Interfaces.DbContext;
+using eShop_Infrastructure.Data;
+using UnitTests.Helpers.BasketBuilder;
 
 namespace IntegrationTests.Repositories.BasketRepositoryTests
 {
     class QuantitiesTests
     {
+        private readonly EShopDbContext _eShopDbContext;
+        private readonly IBasketDbContext _basketDbContext;
+        private readonly BuildBasket buildBasket = new BuildBasket();
+
+        public QuantitiesTests()
+        {
+            
+        }
+
     }
 }
