44424e90a3d7fd8b31c3dd3cfd9fd62497a0ac50
KacperZiarkowski <PLUJKV3@amway.com>
2021-01-12 13:42:37 +0100

    BasketBuilder

---
+++ UnitTests/Helpers/BasketBuilder/BasketBuilder.cs
@@ -0,0 +1,43 @@
+﻿using eShop_ApplicationCore.Model.Basket;
+using System;
+using Force.DeepCloner;
+using System.Diagnostics;
+
+namespace UnitTests.Helpers.BasketBuilder
+{
+    /// <summary>
+    /// Helper class for creating basket for test usage
+    /// </summary>
+    public class BasketBuilder
+    {
+        private Basket _basket;
+
+        private readonly Guid _buyerGuid = Guid.NewGuid();
+
+        private const int _basketId = 1;
+
+
+        public BasketBuilder()
+        {
+            _basket = new Basket()
+            {
+                BuyerGuid = _buyerGuid,
+                Id = _basketId
+            };
+        }
+
+
+        public BasketBuilder BasketWithOneProduct(decimal price)
+        {
+            _basket.AddItem(1, price);
+            return this;
+        }
+
+
+        public Basket Build()
+        {           
+            return _basket.ShallowClone();
+        }
+
+    }
+}
